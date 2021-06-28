e2ad6493cdd629b919145763304223e8f3f4f3c7
davidctn <constantinescudavid50@yahoo.com>
2021-06-28 19:58:50 +0300

    Finish game core logic

---
+++ MyGame/MyGame/app/src/main/java/com/project/mygame/GameBoardAdapter.kt
@@ -0,0 +1,70 @@
+package com.project.mygame
+
+import android.content.Context
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageButton
+import androidx.cardview.widget.CardView
+import androidx.core.content.ContextCompat
+import androidx.core.view.ViewCompat
+import androidx.recyclerview.widget.RecyclerView
+import com.project.mygame.models.BoardSize
+import com.project.mygame.models.MemoryCard
+import kotlin.math.min
+
+class GameBoardAdapter(
+    private val context: Context,
+    private val boardSize: BoardSize,
+    private val cards: List<MemoryCard>,
+    private val cardClickListener: CardClickListener
+) : RecyclerView.Adapter<GameBoardAdapter.ViewHolder>() {
+
+    companion object {
+        private const val MARGIN_SIZE=10
+        private const val TAG="GameBoardAdapter"
+    }
+
+    interface CardClickListener{
+        fun ClickOnCard(position : Int)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        //how to create one view in the rv
+        var cardWidth :Int =parent.width / boardSize.getWitdh() - (2* MARGIN_SIZE)
+        var cardHeight :Int =parent.height / boardSize.getHeight() - (2* MARGIN_SIZE)
+        var cardSideLength :Int = min(cardWidth,cardHeight)
+        val view = LayoutInflater.from(context).inflate(R.layout.memory_card,parent,false)
+        val layoutParams : ViewGroup.MarginLayoutParams =view.findViewById<CardView>(R.id.cardView).layoutParams as ViewGroup.MarginLayoutParams
+        layoutParams.width=cardSideLength
+        layoutParams.height=cardSideLength
+        layoutParams.setMargins(MARGIN_SIZE, MARGIN_SIZE, MARGIN_SIZE, MARGIN_SIZE)
+        return ViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+
+      holder.bind(position)
+    }
+
+    override fun getItemCount() = boardSize.numberCards
+
+    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+        private val imageButton = itemView.findViewById<ImageButton>(R.id.imageButton)
+
+        fun bind(position: Int) {
+            val memoryCard=cards[position]
+            imageButton.setImageResource(if(memoryCard.isFaceUp) memoryCard.identifier else R.drawable.ic_launcher_background)
+            imageButton.alpha = if(memoryCard.isMatched) .5f else 1.0f
+            val colorStateList =  if(memoryCard.isMatched) ContextCompat.getColorStateList(context,R.color.color_gray) else null
+            ViewCompat.setBackgroundTintList(imageButton,colorStateList)//shade
+            imageButton.setOnClickListener {
+                Log.i(TAG,"Clicked on position $position")
+                cardClickListener.ClickOnCard(position)
+            }
+        }
+    }
+
+}
---
+++ MyGame/MyGame/app/src/main/java/com/project/mygame/models/Game.kt
@@ -0,0 +1,66 @@
+package com.project.mygame.models
+
+import com.project.mygame.utility.DEFAULT_ICONS
+
+class Game(private val boardSize: BoardSize) {
+
+    val cards: List<MemoryCard>
+    var numberPairs = 0
+    private var firstCardIndex: Int? = null
+
+    init {
+        val chosenImages: List<Int> = DEFAULT_ICONS.shuffled().take(boardSize.getNumberPairs())
+        val randomizedImages: List<Int> = (chosenImages + chosenImages).shuffled()
+        cards = randomizedImages.map { MemoryCard(it) }
+
+    }
+
+    fun flipCard(position: Int) : Boolean {
+
+        var foundMatch = false
+        val card: MemoryCard = cards[position]
+        if (firstCardIndex == null) {
+            //0 sau 2 carti intoarse
+            restoreDefaultState()
+            firstCardIndex = position
+        }
+        else {
+            //1 carte intoarsa
+            foundMatch = checkCards(firstCardIndex!!,position)
+            firstCardIndex=null
+        }
+        card.isFaceUp = !card.isFaceUp
+        return foundMatch
+    }
+
+    private fun checkCards(firstCardIndex: Int, secondCardIndex: Int): Boolean {
+
+        if(cards[firstCardIndex].identifier != cards[secondCardIndex].identifier){
+            return false
+        }
+        cards[firstCardIndex].isMatched=true
+        cards[secondCardIndex].isMatched=true
+        numberPairs++
+        return true
+
+    }
+
+    private fun restoreDefaultState() {
+
+        for (card in cards) {
+            if (!card.isMatched) {
+                card.isFaceUp = false
+            }
+        }
+    }
+
+    fun winGame(): Boolean {
+
+        return numberPairs == boardSize.getNumberPairs()
+    }
+
+    fun isFaceUp(position : Int): Boolean {
+
+        return cards[position].isFaceUp
+    }
+}
