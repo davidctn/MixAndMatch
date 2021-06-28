325d1e370eb306a3920cb288517607b30b3d6d66
davidctn <constantinescudavid50@yahoo.com>
2021-06-28 20:21:38 +0300

    Minor Fixes

---
+++ MyGame/app/src/main/java/com/project/mygame/ToDelete.kt
@@ -0,0 +1,11 @@
+package com.project.mygame
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ToDelete : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_to_delete)
+    }
+}
