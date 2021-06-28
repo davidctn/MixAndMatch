066ef2389a0489e780c52b7db5bc92fd5947a2b7
Alexandru Georgescu <andyyg477@gmail.com>
2021-06-28 22:44:23 +0300

    Update for create custom game

--- MyGame/app/src/main/java/com/project/mygame/utility/CreateActivity.kt
+++ MyGame/app/src/main/java/com/project/mygame/utility/CreateActivity.kt
@@ -2,11 +2,29 @@ package com.project.mygame.utility
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
 import com.project.mygame.R
+import com.project.mygame.models.BoardSize
 
 class CreateActivity : AppCompatActivity() {
+    private lateinit var boardSize: BoardSize
+    private var numImagesRequired = -1;
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_create)
+
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        boardSize = intent.getSerializableExtra(EXTRA_BOARD_SIZE) as BoardSize
+        numImagesRequired = boardSize.getNumberPairs()
+        supportActionBar?.title = "Choose pics (0 / $numImagesRequired)"
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if(item.itemId == android.R.id.home) {
+            finish()
+            return true
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
