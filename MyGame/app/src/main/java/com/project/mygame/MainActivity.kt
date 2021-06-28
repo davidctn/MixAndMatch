78f31de2e1e44dfee8670721dc385b128236acb8
Alexandru Georgescu <andyyg477@gmail.com>
2021-06-28 20:05:38 +0300

    Added game reset

--- MyGame/app/src/main/java/com/project/mygame/MainActivity.kt
+++ MyGame/app/src/main/java/com/project/mygame/MainActivity.kt
@@ -4,7 +4,11 @@ import android.animation.ArgbEvaluator
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
+import android.view.View
 import android.widget.TextView
+import androidx.appcompat.app.AlertDialog
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.GridLayoutManager
@@ -34,8 +38,14 @@ class MainActivity : AppCompatActivity() {
         rvGameBoard = findViewById(R.id.rvGameBoard)
         tvNumberMoves = findViewById(R.id.tvNumberMoves)
         tvNumberPairs = findViewById(R.id.tvNumberPairs)
-        tvNumberPairs.setTextColor(ContextCompat.getColor(this,R.color.color_progress_0))
         clRoot=findViewById(R.id.clRoot)
+        setupBoard()
+    }
+
+    private fun setupBoard() {
+
+        
+        tvNumberPairs.setTextColor(ContextCompat.getColor(this,R.color.color_progress_0))
         memoryGame = Game(boardSize)
         adapter = GameBoardAdapter(this,boardSize,memoryGame.cards, object : GameBoardAdapter.CardClickListener{
 
@@ -47,6 +57,38 @@ class MainActivity : AppCompatActivity() {
         rvGameBoard.adapter = adapter
         rvGameBoard.setHasFixedSize(true)
         rvGameBoard.layoutManager=GridLayoutManager(this,boardSize.getWitdh())
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_main,menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId){
+            R.id.menuitem_refresh -> {
+                if(!memoryGame.winGame() && memoryGame.getNumberMoves()>0){
+                    displayAlertDialog("Are you sure you want to quit the current game ?",null,View.OnClickListener {
+                        setupBoard()
+                    })
+                }
+                else{
+                    setupBoard()
+                }
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    private fun displayAlertDialog(title : String, view : View?, positiveClickListenener : View.OnClickListener) {
+        AlertDialog.Builder(this)
+            .setTitle(title)
+            .setView(view)
+            .setNegativeButton("Cancel",null)
+            .setPositiveButton("Ok") {_,_->
+            positiveClickListenener.onClick(null)
+            }.show()
+
     }
 
     private fun updateGame(position: Int) {
